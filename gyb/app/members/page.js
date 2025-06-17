103659f4cd81caa715a43ed34a5467e5ce88c99a
ayu-bhar <ayubhar870@gmail.com>
2025-06-17 23:57:12 +0530

    added members and events page

--- gyb/app/members/page.js
+++ gyb/app/members/page.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import TeamCard from '@/components/TeamCard'
 
 function page() {
+  const teamMembers = [
+  {
+    name: "Ayush Bhardwaj",
+    branch: "Computer Science",
+    year: "3rd Year",
+    position: "Tech Lead",
+    image: "/images/img1.jpg",
+    email: "abc.ug.abc@gmail.com"
+  },
+  {
+    name: "Aman Chaudhary",
+    branch: "Electrical Engineering",
+    year: "2nd Year",
+    position: "Event Coordinator",
+    image: "/images/img2.jpg",
+    email: "abc.ug.abc@gmail.com"
+  },
+  {
+    name: "Anantam",
+    branch: "Computer Science",
+    year: "3rd Year",
+    position: "Tech Lead",
+    image: "/images/img3.jpg",
+    email: "abc.ug.abc@gmail.com"
+  },
+  {
+    name: "Shiv Chaudhary",
+    branch: "Electrical Engineering",
+    year: "2nd Year",
+    position: "Event Coordinator",
+    image: "/images/img4.jpg",
+    email: "abc.ug.abc@gmail.com"
+  },
+  {
+    name: "Member Bhardwaj",
+    branch: "Computer Science",
+    year: "3rd Year",
+    position: "Tech Lead",
+    image: "/images/img5.jpg",
+    email: "abc.ug.abc@gmail.com"
+  },
+  {
+    name: "Aman Chaudhary",
+    branch: "Electrical Engineering",
+    year: "2nd Year",
+    position: "Event Coordinator",
+    image: "/images/img2.jpg",
+    email: "abc.ug.abc@gmail.com"
+  },
+];
+
   return (
     <div>
-      <h1>Members Area</h1>
-      <p>Welcome to the Members page of Get Your Book.</p>
+      <h1 className="mt-2 text-4xl my-4 text-center fraunces">Meet the Cool Crew</h1>
+      <TeamCard members={teamMembers}/>
     </div>
   )
 }
---
+++ gyb/components/TeamCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaEnvelope } from "react-icons/fa";
+function TeamCard({ members }) {
+    return (
+        <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
+            {members.map((member,idx) => (
+                <div key={idx} className="bg-yellow-300 py-15 rounded-2xl shadow-xl p-6 text-center transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-full max-w-sm">
+                    <div className="flex justify-center mb-4">
+                        <img
+                            src={member.image}
+                            alt={member.name}
+                            className="w-24 h-24 rounded-full border-4 border-white object-cover shadow-md"
+                        />
+                    </div>
+                    <h2 className="text-xl font-bold text-black my-2">{member.name}</h2>
+                    <p className="text-sm text-black/80 my-4">{member.branch}</p>
+                    <p className="text-sm text-black/70 my-4">{member.year}</p>
+                    <div className="mt-3 font-medium text-black my-4">{member.position}</div>
+                    <p className='flex items-center gap-2 justify-center my-4'><FaEnvelope/>{member.email}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default TeamCard
