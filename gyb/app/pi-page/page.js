7efacbe13ddc9cabfe7528b4956f97b971f2014b
ANANTAM AFTAB <anantama.ug24.cs@nitp.ac.in>
2025-08-08 11:53:39 +0530

    Update page.js

--- gyb/app/pi-page/page.js
+++ gyb/app/pi-page/page.js
@@ -36,7 +36,7 @@ export default function PIPage() {
         <div className="mt-10 bg-[#fdf0e4] p-6 rounded-lg border-l-4 border-[#b4523b] shadow-md text-[#2d1d15] font-serif leading-relaxed text-justify">
           <p className="text-[1.25rem] leading-relaxed tracking-wide">
   
-  At the core of GYB’s sustained growth and meaningful impact lies the guidance and leadership of our esteemed <strong>Professor-in-Charge, Dr. Shiv Shankar Kumar.</strong>
+  At the core of GYB’s sustained growth and meaningful impact lies the guidance and leadership of our esteemed <strong>Professor-in-Charge, Dr. Shiv Shankar Kumar.</strong><br/>
 
 A well-respected faculty member at NIT Patna and a committed mentor to socially conscious student initiatives, Dr. Shiv Shankar Kumar has been instrumental in shaping the club’s direction, values and outreach.
 
